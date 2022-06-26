76931f1f730f88cf96489007b09a2fe0086159d2
chenyonghao <chenyonghao6717@yaocheng.cn>
2022-06-26 16:05:55 +0800

    add 2.19.scm

--- exercises/chapter2/2.19.scm
+++ exercises/chapter2/2.19.scm
@@ -1,3 +1,19 @@
-(define (cc amount coin-values)
+; the code from line 2 to line 19 is located in "SICP" p26
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
   (cond ((= amount 0) 1)
-        ((or (< amount 0) (no-more? coin-values)) 0)
+        ((or (< amount 1) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond (( =kinds-of-coins 1) 1)
+        (( =kinds-of-coins 2) 5)
+        (( =kinds-of-coins 3) 10)
+        (( =kinds-of-coins 4) 25)
+        (( =kinds-of-coins 5) 50)))
