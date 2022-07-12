620ebb02081b23a61e9e72c0868a6ca2db42bb32
chenyonghao <chenyonghao6717@yaocheng.cn>
2022-07-12 13:31:18 +0800

    add 2.40.scm

--- exercises/chapter2/enumerate.scm
+++ exercises/chapter2/enumerate.scm
@@ -3,7 +3,6 @@
       (list)
       (cons low (enumerate-interval (+ low 1) high))))
 
-; enumerate
 (define (enumerate-tree tree)
   (cond ((null? tree) (list))
         ((not (pair? tree)) (list tree))
