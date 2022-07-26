1e22ad03559fcd52a2d5a19a0a124afff1768be1
chenyonghao <chenyonghao6717@yaocheng.cn>
2022-07-26 11:32:25 +0800

    modified:   2.41.scm

--- exercises/chapter2/2.41.scm
+++ exercises/chapter2/2.41.scm
@@ -1,13 +1,20 @@
 ; include enumerate.scm
 ; include accumulate.scm
 
-(define (generate-triple-tuple n)
-  (accumulate append
-             '()
-              (map (lambda (i)
-                     (map (lambda (j)
-                            (cons i j))
-                          (map (lambda (k) (list j k))
-                               (enumerate-interval 1 (- j 1)))))
-                   (
-                   (enumerate-interval 1 n))))
+(define (unique-pair n)
+  (accumulate
+    append
+    '()
+    (map (lambda (j)
+           (map (lambda (k) (list j k))
+                (enumerate-interval 1 (- j 1))))
+         (enumerate-interval 1 n))))
+
+(define (unique-triple-tuple n)
+  (accumulate
+    append
+    '()
+    (map (lambda (i)
+           (map (lambda (j) (cons i j))
+                (unique-pair (- i 1))))
+         (enumerate-interval 1 n))))
