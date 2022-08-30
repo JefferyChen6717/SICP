74fff76dfd9c95530fa6b21e48b4d4df8b9b6aac
chenyonghao <chenyonghao6717@yaocheng.cn>
2022-08-30 22:38:43 +0800

    finish 2.44.scm

--- exercises/chapter2/2.43.scm
+++ exercises/chapter2/2.43.scm
@@ -2,7 +2,7 @@
 ; every possible new-row to every board in (queen-cols (- k 1))
 ; in every recursion it call (queen-cols (- k 1)) once
 
-; but in 2.43' solution, for every possible new-row it generates
+; but in 2.43's solution, for every possible new-row it generates
 ; (queen-cols (- k 1)) once and totally board-size times
 
 ; so if 2.42 costs T, 2.43 costs board-size * board-size * T
